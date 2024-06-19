78a0a69f0b7fe9a6e4cdd297df39c567525ad42b
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-19 17:27:03 -0300

    feat: codigo funcional

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let resultado;
 let numeroUm;
 let numeroDois;
 let input;
+let mmc1;
+let mmc2;
 
 console.log('Olá. Iremos calcular o MMC dos números solicitados.');
 console.log('Informe o primeiro número:');
@@ -14,8 +15,22 @@ process.stdin.on('data', function (data) {
         console.log('Agora nos informe o segundo número:');
     } else {
         numeroDois = input;
-        console.log(numeroUm, numeroDois);
-        process.exit();
+        if (numeroUm > numeroDois) {
+            mmc1 = numeroUm;
+            mmc2 = numeroDois;
+        } else {
+            mmc1 = numeroDois;
+            mmc2 = numeroUm;
+        }
+
+        for (let i = 1; i <= mmc2; i++) {
+            for (let j = 1; j <= mmc1; j++) {
+                if (mmc1 * i == mmc2 * j) {
+                    console.log('O MMC dos números solicotados é', mmc1 * i);
+                    process.exit();
+                }
+            }
+        }
     }
 
 })
